f6044dd0988e92fba6071bf1399d8655b45a3d8e
Anna De Lise <anna_delise@libero.it>
2025-08-01 12:03:42 +0200

    add fetch 5 days

--- src/components/MeteoFive.jsx
+++ src/components/MeteoFive.jsx
@@ -1,7 +1,35 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const MeteoFive = () => {
     const { cityId } = useParams();
+  const [meteo, setMeteo] = useState("");
+  const [isError, setIsError] = useState(false);
+
+     useEffect(() => {
+        if (!cityId) return;
+        fetch(
+            `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=b425d1c0dd147f0e48723fb6517a4691`
+        )
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              throw new Error("Errore nel recupero dei dati");
+            }
+          })
+          .then((data) => {
+            setMeteo(data);
+            setIsError(false);
+          })
+          .catch((error) => {
+            console.error("Errore", error);
+            setIsError(true);
+          });
+      }, []);
+    
+
+
     return (
         <>
             <h1 className="title">METEO FIVE</h1>
